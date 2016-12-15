6ef22795624b86e807392f2484e9a0ec8b1972d3
Jenny Nilsson <jenny.nilsson@jayway.com>
2016-12-15 13:05:13 +0100

    Day 15

---
+++ 15/day_15.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const File = require('fs');
+const lines = File.readFileSync("input.txt", "utf-8").trim().split("\n");
+const instructions = {};
+
+lines.forEach(line => {
+    let match = line.match(/^Disc #(\d) has (\d+) positions; at time=(\d+), it is at position (\d+)./);
+    let instance = {
+        position: parseInt(match[4]),
+        positions: parseInt(match[2]),
+        valid: (t) => (t + instance.position) % instance.positions === 0
+    };
+    instructions[match[1]] = instance;
+});
+
+function findStartTime(instructions, nbrOfDiscs) {
+    let startTime = 0;
+    let foundTime = false;
+    while (!foundTime) {
+        foundTime = true;
+        for (let i = 1; i <= nbrOfDiscs; i++) {
+            if (!instructions[i].valid(startTime + i)) {
+                foundTime = false;
+                break;
+            }
+        }
+        if (!foundTime) startTime++;
+    }
+    return startTime;
+}
+
+console.log("Part 1 : ", findStartTime(instructions, 6));
+console.log("Part 2 : ", findStartTime(instructions, 7));
